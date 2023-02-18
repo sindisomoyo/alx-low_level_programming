af1b7afce2aad5927a53faf3855ff8584f3f5aea
moyoviolet2@gmail.com <moyoviolet2@gmail.com>
2023-02-18 03:29:56 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
-#include <stdio.h>               /**                               * main - A program that prints a line with puts function          * Return: 0 (success)            */                              int main(void)                   {                                puts("\"Programming is like building a multilingual puzzle");     return (0);                      }
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
